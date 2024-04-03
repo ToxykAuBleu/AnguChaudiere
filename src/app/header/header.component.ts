f3dc1e376d6e4fd42fd5194199057c9f8a544f05
ToxykAuBleu <foissac.mathieu@gmail.com>
2024-04-03 10:45:03 +0200

    🔥 Ajout de l'affichage conditionnel de la navbar.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   title: string = "AnguChaudière";
+  public currentRoute!: string;  
+
+
+  constructor(private router: Router) {
+    // Ajout d'un event pour connaître en temps réel la route.
+    // (Utilisé pour le style de la navbar)
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url;
+      }
+    });
+  }
 }
